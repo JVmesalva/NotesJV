1000764ee0078e7ec60e934df43263214dd06ac2
JVmesalva <jvmesalva@gmail.com>
2026-08-28 18:01:23 -0300

    Encode recovery email in confirmation route

--- app/(auth)/forgot-password/_hooks/use-forgot-password.ts
+++ app/(auth)/forgot-password/_hooks/use-forgot-password.ts
@@ -15,9 +15,8 @@ export default function useForgotPassword() {
     const res = await resetPasswordAsync(email)
 
     if (res?.error) form.setError("root.apiError", { message: res.error })
-    else router.replace(`/forgot-password-verify?mailto=${email}`)
+    else router.replace(`/forgot-password-verify?mailto=${encodeURIComponent(email)}`)
   })
-
 
   return {
     form,
