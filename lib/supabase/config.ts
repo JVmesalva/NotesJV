b39f997127617e669a3d5a8e58baccff95bc1cb1
JVmesalva <jvmesalva@gmail.com>
2026-08-28 17:16:24 -0300

    Use jvlc.cc as primary app URL

--- lib/supabase/config.ts
+++ lib/supabase/config.ts
@@ -1,3 +1,3 @@
-export const appUrl = "https://notes.jvlc.cc"
+export const appUrl = "https://jvlc.cc"
 export const supabaseUrl = "https://eaidmzuwhzdcepedenlo.supabase.co"
 export const supabasePublishableKey = "sb_publishable_2Hte0aDw-WFsQGR7Inxfkg_sRsbwpqp"
